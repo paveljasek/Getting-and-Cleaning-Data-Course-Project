9c5f44cc133adc3b68c4b0ce4bdc24dfab93bdc7
pavel jašek <paveljasek@gmail.com>
2014-12-21 20:49:07 +0100

    tasks 1 to 5

--- CodeBook.md
+++ CodeBook.md
@@ -1,5 +1,21 @@
 Human Activity Recognition Using Smartphones Data Set 
 ========================================================
+Dataset from http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
 
-Describe variables
-Transformations performed to clean up the data
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+
+#Describe variables
+
+For each record in the dataset it is provided: 
+- Triaxial acceleration from the accelerometer (total acceleration) and the estimated body acceleration. 
+- Triaxial Angular velocity from the gyroscope. 
+- A 561-feature vector with time and frequency domain variables. 
+- Its activity label. 
+- An identifier of the subject who carried out the experiment.
+
+
+- Features are normalized and bounded within [-1,1].
+- Each feature vector is a row on the text file.
+
+#Transformations performed to clean up the data
--- run_analysis.R
+++ run_analysis.R
@@ -13,3 +13,65 @@
 
 # Merges the training and the test sets to create one data set ------------
 
+# read subjects
+subject.test <- read.table("UCI HAR Dataset//test/subject_test.txt")
+subject.train <- read.table("UCI HAR Dataset//train/subject_train.txt")
+
+unique(subject.test)
+unique(subject.train)
+
+subject <- rbind(subject.test, subject.train)
+
+# sets
+set.test <- read.table("UCI HAR Dataset//test/X_test.txt")
+set.train <- read.table("UCI HAR Dataset//train/X_train.txt")
+
+set <- rbind(set.test, set.train)
+
+# labels
+label.test <- read.table("UCI HAR Dataset//test/y_test.txt")
+label.train <- read.table("UCI HAR Dataset//train/y_train.txt")
+
+label <- rbind(label.test, label.train)
+
+# features
+features <- read.table("UCI HAR Dataset//features.txt")
+
+
+# 2. Extracts only the measurements on the mean and standard devia --------
+
+# find indices and names for measurements that contain -mean() or -std()
+measurements <- features[grep("-(mean|std)\\(", features$V2),]
+# names without brackets and minus signs
+measurements$cleanName <- gsub("\\(|\\)|-","_",measurements$V2)
+measurements$cleanName <- gsub("__$","",measurements$cleanName)
+
+#measurements$V1 contains indices for required measures
+set.subset <- set[,measurements$V1]
+
+# 3. Uses descriptive activity names to name the activities --------
+
+
+activities <- read.table("UCI HAR Dataset//activity_labels.txt")
+# unique(label)
+label.names <- merge(label, activities, by="V1", all.x=T, sort=F)
+# head(label.names)
+
+
+# 4. Appropriately labels the data set with descriptive names --------
+
+# edit column names
+colnames(set.subset) <- measurements$cleanName
+
+# data frame of activities, subjects and selected measures
+df <- data.frame(activity = label.names$V2, subject = subject$V1, set.subset)
+# head(df)
+
+
+# 5. From the data set in step 4, creates a second, independent --------
+# tidy data set with the average of each variable for each activity and each subject.
+
+str(df)
+averages <- aggregate(. ~ activity + subject, df, FUN=mean)
+
+write.table(averages, 'averages.txt', row.name=F)
